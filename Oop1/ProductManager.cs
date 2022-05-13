d90aaab984e7f610fc62aec5d50996c903ecf85f
okan-87 <okankaplan1907@gmail.com>
2022-05-13 16:29:44 +0200

    Add project files.

---
+++ Oop1/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oop1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine("Added : " + product.ProductName);
+        }
+        public void Updated(Product product)
+        {
+            Console.WriteLine("Updated : " + product.ProductName);
+        }
+        public void Delete(Product product)
+        {
+            Console.WriteLine("Deleted : " + product.ProductName);
+        }
+    }
+}
---
+++ Oop1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Oop1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 10;
+            product1.ProductName = "Table";
+            product1.UnitPrice = 500;
+            product1.UnitInStock = 3;
+
+            Product product2 = new Product
+            {
+                Id = 2,
+                CategoryId = 20,
+                UnitPrice = 35,
+                ProductName = "Pencil",
+                UnitInStock = 35
+            };
+
+            ProductManager productmanager = new ProductManager();
+            productmanager.Add(product1);
+            productmanager.Add(product2);
+        }
+    }
+}
